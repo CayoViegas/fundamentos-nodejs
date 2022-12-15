98e9fab53a82fe7de36743f84334864844bfe19d
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2022-12-15 03:46:54 -0300

    parametros adicionados

--- src/index.js
+++ src/index.js
@@ -2,19 +2,27 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/courses", (request, response) => {
+	const query = request.query;
+	console.log(query);
 	return response.json({
 		cursos: ["CC", "EE"],
 	});
 });
 
 app.post("/courses", (request, response) => {
+	const body = request.body;
+	console.log(body);
 	return response.json({
 		cursos: ["CC", "EE", "AeM"],
 	});
 });
 
 app.put("/courses/:id", (request, response) => {
+	const { id } = request.params;
+	console.log(id);
 	return response.json({
 		cursos: ["Ciência da Computação", "EE", "AeM"],
 	});
