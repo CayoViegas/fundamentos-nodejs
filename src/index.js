2752b03d2a083803d8e6ab2930f8f98af3d3dbc8
CayoViegas <cayo.viegas@ccc.ufcg.edu.br>
2022-12-15 03:16:07 -0300

    métodos HTTP adicionados

--- src/index.js
+++ src/index.js
@@ -2,8 +2,34 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-	return response.json({ message: "Hello World!" });
+app.get("/courses", (request, response) => {
+	return response.json({
+		cursos: ["CC", "EE"],
+	});
+});
+
+app.post("/courses", (request, response) => {
+	return response.json({
+		cursos: ["CC", "EE", "AeM"],
+	});
+});
+
+app.put("/courses/:id", (request, response) => {
+	return response.json({
+		cursos: ["Ciência da Computação", "EE", "AeM"],
+	});
+});
+
+app.patch("/courses/:id", (request, response) => {
+	return response.json({
+		cursos: ["Ciência da Computação", "Engenharia Elétrica", "AeM"],
+	});
+});
+
+app.delete("/courses/:id", (request, response) => {
+	return response.json({
+		cursos: ["Ciência da Computação", "Engenharia Elétrica"],
+	});
 });
 
 app.listen(3333);
